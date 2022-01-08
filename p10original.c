b95a59c6bf25d21734d48e85ecf6074389a1a89f
RakshakMU <97020781+RakshakMU@users.noreply.github.com>
2022-01-08 17:32:03 +0000

    p1 original

--- p10original.c
+++ p10original.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
+int main(){
+  int l,b,a;
+  printf("enter the values of l and b/n");
+  scanf("%d%d",&l,&b);
+  a=l*b
+  printf("area of rectangle=%d/n",a);
+}
 
--- p1original.c
+++ p1original.c
@@ -1,2 +1,8 @@
-
+#include<stdio.h>
+int main(){
+  int l,b,a;
+  printf("enter the values of l and b\n");
+  scanf("%d%d",&l,&b);
+  a=l*b;
+  printf("area of rectangle = %d\n",a);
 }
